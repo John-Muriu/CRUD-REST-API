c0b736be419fe1b326ad174fe13d4b270fa84677
john <johnmuriw@gmail.com>
2023-02-14 14:35:55 +0300

    added delete user function

--- controllers/user.js
+++ controllers/user.js
@@ -82,3 +82,28 @@ exports.updateUser =(req,res next) => {
     .catch(err => console.log(err));
      
 }
+
+
+// delete Users
+
+exports.deleteUsers =(req,res, next ) => {
+
+    const userId = req.param.userId;
+    User.findByPk(userId)
+    .then(user =>{
+        if (!user) {
+            return res.status(404).json({message:'user not found'})
+
+        }
+        return User.destroy({
+            where:{
+                id:userId
+            }
+        });
+    })
+    .then (result => {
+        res.status(200).json({message: 'user deleted!!'});
+    })
+    .catch(err => console.log(err));
+
+}
